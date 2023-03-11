cf37500447751107ff6f67f79cebc946140a7ca8
cstile2 <cstile2@illinois.edu>
2023-03-11 00:31:16 -0600

    Cleaned up code, added comments

--- docs/webgl-demo.js
+++ docs/webgl-demo.js
@@ -1,18 +1,16 @@
-//keep track of whether the mouse stuff
+//keep track of the mouse stuff
 let mouse_pos = vec2.create();
-window.addEventListener("pointermove", (event) => {
-  mouse_pos = vec2.fromValues(event.clientX, event.clientY);//{ x: event.clientX, y: event.clientY };
-});
 let mouse_down = false;
 let penpressure = 0;
+window.addEventListener("pointermove", (event) => {
+  mouse_pos = vec2.fromValues(event.clientX, event.clientY);
+  penpressure = event.pressure;
+});
 window.addEventListener("pointerdown", () => {
   mouse_down = true;
 });
 window.addEventListener("pointerup", () => {
   mouse_down = false;
-});
-window.addEventListener("pointermove", (event) => {
-  penpressure = event.pressure;
 });
 let scroll_y = 0;
 let scroll_x = 0;
@@ -34,14 +32,15 @@ window.addEventListener("wheel", (event) =>
 });
 
 //disable scrolling on mobile devices
-document.addEventListener('touchmove', (e) => { e.preventDefault(); }, { passive:false, premultipliedAlpha: false});
+document.addEventListener('touchmove', (e) => { e.preventDefault(); }, { passive:false});
 
 //initialize the canvas to the size of the screen
 document.getElementById("glcanvas").height = window.innerHeight;
 document.getElementById("glcanvas").width = window.innerWidth;
 
+//initialize webgl
 const canvas = document.querySelector("canvas");
-const gl = canvas.getContext("webgl", { antialias: false });
+const gl = canvas.getContext("webgl", { antialias: false, premultipliedAlpha: false });
 if (!gl) {
   throw new Error("Webgl is not supported");
 }
@@ -72,13 +71,6 @@ let cube_vertex_data = [
   0.5, -0.5, 0.5, 0.5, -0.5, -0.5, -0.5, -0.5, 0.5, -0.5, -0.5, 0.5, 0.5, -0.5,
   -0.5, -0.5, -0.5, -0.5,
 ];
-let cube_color_data = [];
-for (let face = 0; face < 6; face++) {
-  let faceColor = [Math.random(), Math.random(), Math.random()];
-  for (let vertex = 0; vertex < 6; vertex++) {
-    cube_color_data.push(...faceColor);
-  }
-}
 
 //buffers
 let cube_position_buffer = gl.createBuffer();
@@ -89,16 +81,13 @@ gl.bufferData(
   gl.STATIC_DRAW
 );
 
-let cube_color_buffer = gl.createBuffer();
-gl.bindBuffer(gl.ARRAY_BUFFER, cube_color_buffer);
-gl.bufferData(
-  gl.ARRAY_BUFFER,
-  new Float32Array(cube_color_data),
-  gl.STATIC_DRAW
-);
-
+//the geometry of the pencil strokes
 let stroke_data = [];
 
+//buffer for the pencil strokes - for webgl
+let pencil_stroke_buffer = gl.createBuffer();
+
+//clear the drawing on spacebar pressed
 window.addEventListener('keypress', function (e) {
   if (e.key == ' ')
   {
@@ -106,77 +95,10 @@ window.addEventListener('keypress', function (e) {
   }
 }, false);
 
-let pencil_stroke_buffer = gl.createBuffer();
-gl.bindBuffer(gl.ARRAY_BUFFER, pencil_stroke_buffer);
-gl.bufferData(
-  gl.ARRAY_BUFFER,
-  new Float32Array(stroke_data),
-  gl.STATIC_DRAW
-)
-
 //vertex shader
-let cube_vertex_shader = gl.createShader(gl.VERTEX_SHADER);
+let brush_stroke_vert = gl.createShader(gl.VERTEX_SHADER);
 gl.shaderSource(
-  cube_vertex_shader,
-  `
-precision mediump float;
-attribute vec3 position;
-attribute vec3 color;
-varying vec3 vColor;
-
-uniform mat4 matrix;
-
-void main()
-{
-  vColor = color;
-  //outputs to clip-space?
-  gl_Position = matrix * vec4(position, 1);
-}
-`
-);
-gl.compileShader(cube_vertex_shader);
-
-//fragment shader
-let cube_frag_shader = gl.createShader(gl.FRAGMENT_SHADER);
-gl.shaderSource(
-  cube_frag_shader,
-  `
-precision mediump float;
-varying vec3 vColor;
-
-void main()
-{
-  gl_FragColor = vec4(vColor, 1);
-}
-`
-);
-gl.compileShader(cube_frag_shader);
-
-//link the shader program
-let cube_shader_program = gl.createProgram();
-gl.attachShader(cube_shader_program, cube_vertex_shader);
-gl.attachShader(cube_shader_program, cube_frag_shader);
-gl.linkProgram(cube_shader_program);
-
-//attributes - these are in the vertex shader
-let cube_pos_attrib_loc = gl.getAttribLocation(cube_shader_program, "position");
-gl.enableVertexAttribArray(cube_pos_attrib_loc);
-gl.bindBuffer(gl.ARRAY_BUFFER, cube_position_buffer);
-gl.vertexAttribPointer(cube_pos_attrib_loc, 3, gl.FLOAT, false, 0, 0);
-
-let cube_col_attrib_loc = gl.getAttribLocation(cube_shader_program, "color");
-gl.enableVertexAttribArray(cube_col_attrib_loc);
-gl.bindBuffer(gl.ARRAY_BUFFER, cube_color_buffer);
-gl.vertexAttribPointer(cube_col_attrib_loc, 3, gl.FLOAT, false, 0, 0);
-
-let cube_matrix_uniform = gl.getUniformLocation(cube_shader_program, "matrix");
-
-//creating a second shader program to test rendering multiple================================================================================
-
-//vertex shader
-let v_shader_2 = gl.createShader(gl.VERTEX_SHADER);
-gl.shaderSource(
-  v_shader_2,
+  brush_stroke_vert,
 `
 precision mediump float;
 attribute vec3 position;
@@ -188,41 +110,39 @@ void main()
 }
 `
 );
-gl.compileShader(v_shader_2);
+gl.compileShader(brush_stroke_vert);
 
 //fragment shader
-let f_shader_2 = gl.createShader(gl.FRAGMENT_SHADER);
+let brush_stroke_fragm = gl.createShader(gl.FRAGMENT_SHADER);
 gl.shaderSource(
-  f_shader_2,
+  brush_stroke_fragm,
 `
 precision mediump float;
 uniform vec3 color;
 
 void main()
 {
-  gl_FragColor = vec4(0, 0, 0, 1.);
+  gl_FragColor = vec4(color, 1);
 }
 `
 );
-gl.compileShader(f_shader_2);
+gl.compileShader(brush_stroke_fragm);
 
 //link the shader program
-let shader_program_2 = gl.createProgram();
-gl.attachShader(shader_program_2, v_shader_2);
-gl.attachShader(shader_program_2, f_shader_2);
-gl.linkProgram(shader_program_2);
+let brush_stroke_shader_program = gl.createProgram();
+gl.attachShader(brush_stroke_shader_program, brush_stroke_vert);
+gl.attachShader(brush_stroke_shader_program, brush_stroke_fragm);
+gl.linkProgram(brush_stroke_shader_program);
 
 //attributes - these are in the vertex shader
-let pos_attrib_loc_2 = gl.getAttribLocation(shader_program_2, "position");
+let pos_attrib_loc_2 = gl.getAttribLocation(brush_stroke_shader_program, "position");
 gl.enableVertexAttribArray(pos_attrib_loc_2);
 gl.bindBuffer(gl.ARRAY_BUFFER, cube_position_buffer);
 gl.vertexAttribPointer(pos_attrib_loc_2, 3, gl.FLOAT, false, 0, 0);
 
-let matrix_2 = gl.getUniformLocation(shader_program_2, "matrix");
-
-let color_uniform = gl.getUniformLocation(shader_program_2, "color");
-
-//=========================================================================================
+//uniforms - these are in the vertex and fragment shader
+let matrix_uniform = gl.getUniformLocation(brush_stroke_shader_program, "matrix");
+let color_uniform = gl.getUniformLocation(brush_stroke_shader_program, "color");
 
 //initialize matrices
 let cube_model_mat = mat4.create();
@@ -237,6 +157,8 @@ mat4.invert(pixel_to_clip, pixel_to_clip);
 let clip_to_world = mat4.create();
 mat4.scale(clip_to_world, clip_to_world, [canvas.width/2, canvas.height/2, 1]);
 
+let world_to_clip = mat4.create();
+
 //initialize some state for the animation loop
 gl.enable(gl.DEPTH_TEST);
 gl.clearColor(0.9, 0.9, 0.8, 1.0);
@@ -247,23 +169,26 @@ let prev_mouse_down = mouse_down; //useful to know when the mouse starts to draw
 let prev_worldspace_mousepos = vec2.create();
 let prev_adj = vec2.create();
 
-let world_to_clip = mat4.create();
-
+//update function
 function Update()
 {
   time += 0.01;
 
-  zoom *= Math.E**(zoom_delta/400);
+  zoom *= Math.E**(zoom_delta/800);
 
+  //scale camera
   clip_to_world[0] = canvas.width * zoom;
   clip_to_world[5] = canvas.height * zoom;
+  //translate camera
   clip_to_world[12] += scroll_x * zoom ;
   clip_to_world[13] -= scroll_y * zoom ;
+  //reset variables after use
   scroll_y = 0;
   scroll_x = 0;
   zoom_delta = 0;
   rotate = 0;
 
+  //invert camera's transform matrix
   world_to_clip = mat4.create();
   mat4.invert(world_to_clip, clip_to_world);
 
@@ -321,23 +246,26 @@ function Update()
   )
 }
 
+//drawing function
 function Draw()
 {
   gl.clear(gl.COLOR_BUFFER_BIT);
 
-  gl.useProgram(shader_program_2);
+  gl.uniform3fv(color_uniform, [0,0.1,0.3]);
+
+  gl.useProgram(brush_stroke_shader_program);
   gl.enableVertexAttribArray(pos_attrib_loc_2);
   gl.bindBuffer(gl.ARRAY_BUFFER, cube_position_buffer);
   gl.vertexAttribPointer(pos_attrib_loc_2, 3, gl.FLOAT, false, 0, 0);
   let clipspace_matrix = mat4.create();
   mat4.multiply(clipspace_matrix, world_to_clip, cube_model_mat);
-  gl.uniformMatrix4fv(matrix_2, false, clipspace_matrix);
+  gl.uniformMatrix4fv(matrix_uniform, false, clipspace_matrix);
   gl.drawArrays(gl.TRIANGLES, 0, cube_vertex_data.length / 3);
 
   gl.enableVertexAttribArray(pos_attrib_loc_2);
   gl.bindBuffer(gl.ARRAY_BUFFER, pencil_stroke_buffer);
   gl.vertexAttribPointer(pos_attrib_loc_2, 2, gl.FLOAT, false, 0, 0);
-  gl.uniformMatrix4fv(matrix_2, false, world_to_clip);
+  gl.uniformMatrix4fv(matrix_uniform, false, world_to_clip);
   gl.drawArrays(gl.TRIANGLES, 0, stroke_data.length / 2);
 }
 
@@ -362,7 +290,6 @@ window.addEventListener("resize", () => {
   mat4.invert(pixel_to_clip, pixel_to_clip);
 
   let temp = [clip_to_world[12],clip_to_world[13],clip_to_world[14]];//preserve camera position
-  let old_scale = [clip_to_world[0], clip_to_world[5]];
 
   clip_to_world = mat4.create();
   mat4.scale(clip_to_world, clip_to_world, [canvas.width/2, canvas.height/2, 1]);
