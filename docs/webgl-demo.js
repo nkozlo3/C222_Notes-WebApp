c721294ec435da3700711851e1bc557192dfc334
cstile2 <cstile2@illinois.edu>
2023-03-12 03:27:31 -0500

    Added pixel brush and pixel canvas
    Now pixel brush and vector brush can be used seamlessly by pressing '2'.

--- docs/webgl-demo.js
+++ docs/webgl-demo.js
@@ -95,6 +95,10 @@ window.addEventListener('keypress', function (e) {
   {
     stroke_data = [];
   }
+  if (e.key == '2')
+  {
+    drawing_tool = !drawing_tool;
+  }
 }, false);
 
 //vertex shader
@@ -120,11 +124,11 @@ gl.shaderSource(
   brush_stroke_fragm,
 `
 precision mediump float;
-uniform vec3 color;
+uniform vec4 color;
 
 void main()
 {
-  gl_FragColor = vec4(color, 1);
+  gl_FragColor = color;
 }
 `
 );
@@ -204,6 +208,145 @@ gl.vertexAttribPointer(sheet_uv_attrib, 2, gl.FLOAT, false, 0, 0);
 //uniforms
 let sheet_matrix_uniform = gl.getUniformLocation(sheet_shader_program, "matrix");
 
+
+
+
+
+
+
+
+
+
+
+
+//===================================================================================================================================
+//vertex shader
+let pixel_sheet_vertex_shader = gl.createShader(gl.VERTEX_SHADER);
+gl.shaderSource(pixel_sheet_vertex_shader,
+`
+precision mediump float;
+attribute vec3 position;
+attribute vec2 uv_coord;
+varying vec2 uv;
+
+uniform mat4 matrix;
+
+void main()
+{
+  gl_Position = matrix * vec4(position, 1);
+  uv = uv_coord;
+}
+`
+);
+gl.compileShader(pixel_sheet_vertex_shader);
+
+//fragment shader
+let pixel_sheet_fragment_shader = gl.createShader(gl.FRAGMENT_SHADER);
+gl.shaderSource(pixel_sheet_fragment_shader,
+`
+precision mediump float;
+uniform vec3 color;
+varying vec2 uv;
+
+uniform vec2 cursor_pos;
+uniform vec2 prev_cursor_pos;
+
+float sdf_circle(vec2 p, vec2 c, float r)
+{
+  return length(p - c) - r;
+}
+
+float sdf_cap(vec2 p, vec2 a, vec2 b, float r)
+{
+  vec2 offset = p - a;
+  vec2 line = b - a;
+  float t = clamp(dot(offset, line) / dot(line, line), 0., 1.);
+  vec2 closest = a + line * t;
+  return sdf_circle(p, closest, r);
+}
+
+void main()
+{
+  float dist = sdf_cap(gl_FragCoord.xy, prev_cursor_pos, cursor_pos, 3. - 0.5);
+  dist = 1. - dist;
+  gl_FragColor = vec4(color, dist);
+}
+`
+);
+gl.compileShader(pixel_sheet_fragment_shader);
+
+//link the shader program
+let pixel_sheet_shader_program = gl.createProgram();
+gl.attachShader(pixel_sheet_shader_program, pixel_sheet_vertex_shader);
+gl.attachShader(pixel_sheet_shader_program, pixel_sheet_fragment_shader);
+gl.linkProgram(pixel_sheet_shader_program);
+
+//attributes - these are in the vertex shader
+let pixel_sheet_vertex_attrib = gl.getAttribLocation(pixel_sheet_shader_program, "position");
+gl.enableVertexAttribArray(pixel_sheet_vertex_attrib);
+gl.bindBuffer(gl.ARRAY_BUFFER, square_vertex_buffer);
+gl.vertexAttribPointer(pixel_sheet_vertex_attrib, 3, gl.FLOAT, false, 0, 0);
+let pixel_sheet_uv_attrib = gl.getAttribLocation(pixel_sheet_shader_program, "uv_coord");
+gl.enableVertexAttribArray(pixel_sheet_uv_attrib);
+gl.bindBuffer(gl.ARRAY_BUFFER, square_uv_buffer);
+gl.vertexAttribPointer(pixel_sheet_uv_attrib, 2, gl.FLOAT, false, 0, 0);
+
+
+//uniforms - these are in the vertex and fragment shader
+let pixel_sheet_matrix_uniform = gl.getUniformLocation(pixel_sheet_shader_program, "matrix");
+let pixel_sheet_color_uniform = gl.getUniformLocation(pixel_sheet_shader_program, "color");
+let pixel_sheet_cursor_pos_uniform = gl.getUniformLocation(pixel_sheet_shader_program, "cursor_pos");
+let pixel_sheet_prev_cursor_pos_uniform = gl.getUniformLocation(pixel_sheet_shader_program, "prev_cursor_pos");
+//================================================================================================================================================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// create to render to
+const frame_buffer_width = canvas.width;
+const frame_buffer_height = canvas.height;
+const render_texture = gl.createTexture();
+gl.bindTexture(gl.TEXTURE_2D, render_texture);
+
+  // define size and format of level 0
+  const level = 0;
+  const internalFormat = gl.RGBA;
+  const border = 0;
+  const format = gl.RGBA;
+  const type = gl.UNSIGNED_BYTE;
+  const data = null;
+  gl.texImage2D(gl.TEXTURE_2D, level, internalFormat, frame_buffer_width, frame_buffer_height, border, format, type, data);
+ 
+  // set the filtering so we don't need mips
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+
+  // Create and bind the framebuffer
+  const fb = gl.createFramebuffer();
+  gl.bindFramebuffer(gl.FRAMEBUFFER, fb);
+  
+  // attach the texture as the first color attachment
+  const attachmentPoint = gl.COLOR_ATTACHMENT0;
+  gl.framebufferTexture2D(gl.FRAMEBUFFER, attachmentPoint, gl.TEXTURE_2D, render_texture, level);
+
+
+gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
 //load image
 let sheet_texture = gl.createTexture();
 gl.bindTexture(gl.TEXTURE_2D, sheet_texture);
@@ -214,6 +357,7 @@ image.addEventListener('load', function() {
   // Now that the image has loaded make copy it to the texture.
   gl.bindTexture(gl.TEXTURE_2D, sheet_texture);
   gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+  console.log("image loaded");
 });
 
 //initialize matrices
@@ -232,6 +376,14 @@ mat4.invert(pixel_to_clip, pixel_to_clip);
 
 let clip_to_world = mat4.create();
 mat4.scale(clip_to_world, clip_to_world, [canvas.width/2, canvas.height/2, 1]);
+window.addEventListener('keypress', function (e) {
+  if (e.key == '1')
+  {
+    clip_to_world[12] = 0;
+    clip_to_world[13] = 0;
+    zoom = 1;
+  }
+}, false);
 
 let world_to_clip = mat4.create();
 
@@ -239,13 +391,15 @@ let world_to_clip = mat4.create();
 gl.enable(gl.DEPTH_TEST);
 gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
 gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-gl.clearColor(0.7, 0.7, 0.7, 1.0);
+gl.clearColor(0.13, 0.15, 0.2, 1.0);
 gl.enable(gl.BLEND);
 gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
 let time = 0;
 let prev_mouse_down = mouse_down; //useful to know when the mouse starts to draw
 let prev_worldspace_mousepos = vec2.create();
 let prev_adj = vec2.create();
+let worldspace_mousepos = vec2.create();
+let drawing_tool = 0;
 
 //update function
 function Update()
@@ -270,7 +424,7 @@ function Update()
   world_to_clip = mat4.create();
   mat4.invert(world_to_clip, clip_to_world);
 
-  let worldspace_mousepos = vec2.clone(mouse_pos);
+  worldspace_mousepos = vec2.clone(mouse_pos);
   vec2.transformMat4(worldspace_mousepos, worldspace_mousepos, pixel_to_clip);
   vec2.transformMat4(worldspace_mousepos, worldspace_mousepos, clip_to_world);
 
@@ -282,10 +436,10 @@ function Update()
   let adj = vec2.create();
   vec2.sub(adj, worldspace_mousepos, prev_worldspace_mousepos);
   vec2.normalize(adj,adj);
-  vec2.scale(adj,adj,3);
+  vec2.scale(adj,adj,6);
   adj = vec2.fromValues(adj[1], -adj[0]);
   let moved = Math.abs(worldspace_mousepos[0] - prev_worldspace_mousepos[0]) + Math.abs(worldspace_mousepos[1] - prev_worldspace_mousepos[1]) > 0;
-  if(mouse_down && moved)
+  if(mouse_down && moved && drawing_tool == 1)
   {
     if(stroke_data.length < 6 || !prev_mouse_down)
     {
@@ -317,21 +471,46 @@ function Update()
   }
   prev_mouse_down = mouse_down;
 
-  gl.bufferData(
-  gl.ARRAY_BUFFER,
-  new Float32Array(stroke_data),
-  gl.STATIC_DRAW
-  )
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(stroke_data), gl.STATIC_DRAW);
 }
 
+let pixel_cursor = vec2.create();
+let prev_pixel_cursor = vec2.create();
 //drawing function
 function Draw()
 {
+  //draw to the framebuffer:
+  gl.bindTexture(gl.TEXTURE_2D, sheet_texture);
+  gl.bindFramebuffer(gl.FRAMEBUFFER, fb);
+  gl.viewport(0, 0, frame_buffer_width, frame_buffer_height);
+
+  gl.useProgram(pixel_sheet_shader_program);
+
+  gl.uniform3fv(pixel_sheet_color_uniform, [0.6,1,0.6]);
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, square_vertex_buffer);
+  gl.vertexAttribPointer(pixel_sheet_vertex_attrib, 3, gl.FLOAT, false, 0, 0);
+  gl.uniformMatrix4fv(pixel_sheet_matrix_uniform, false, clip_to_world);
+  pixel_cursor = vec2.fromValues((worldspace_mousepos[0] + canvas.width)/2, (worldspace_mousepos[1] + canvas.height)/2);
+  gl.uniform2fv(pixel_sheet_cursor_pos_uniform, pixel_cursor);
+  gl.uniform2fv(pixel_sheet_prev_cursor_pos_uniform, prev_pixel_cursor);
+  prev_pixel_cursor = vec2.fromValues(pixel_cursor[0], pixel_cursor[1]);
+  if(mouse_down && drawing_tool == 0)
+  {
+    gl.drawArrays(gl.TRIANGLES, 0, square_vertex_array.length / 3);
+  }
+
+  //draw to the actual screen:
+
+  gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+  gl.bindTexture(gl.TEXTURE_2D, render_texture);
+
+  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
   gl.clear(gl.COLOR_BUFFER_BIT);
 
   gl.useProgram(sheet_shader_program);
 
-  gl.enableVertexAttribArray(sheet_position_attrib);
   gl.bindBuffer(gl.ARRAY_BUFFER, square_vertex_buffer);
   gl.vertexAttribPointer(sheet_position_attrib, 3, gl.FLOAT, false, 0, 0);
   let tempyy = mat4.create();
@@ -340,17 +519,17 @@ function Draw()
   gl.drawArrays(gl.TRIANGLES, 0, square_vertex_array.length / 3);
 
   gl.useProgram(brush_stroke_shader_program);
-  gl.uniform3fv(brush_stroke_color_uniform, [0.9,0.9,0.9]);
+  gl.uniform4fv(brush_stroke_color_uniform, [0,1,1,0.2]);
 
-  gl.enableVertexAttribArray(brush_stroke_position_attrib);
   gl.bindBuffer(gl.ARRAY_BUFFER, square_vertex_buffer);
   gl.vertexAttribPointer(brush_stroke_position_attrib, 3, gl.FLOAT, false, 0, 0);
   let clipspace_matrix = mat4.create();
   mat4.multiply(clipspace_matrix, world_to_clip, square_model_mat);
   gl.uniformMatrix4fv(brush_stroke_matrix_uniform, false, clipspace_matrix);
-  gl.drawArrays(gl.TRIANGLES, 0, square_vertex_array.length / 3);
+  //gl.drawArrays(gl.TRIANGLES, 0, square_vertex_array.length / 3);
 
-  gl.enableVertexAttribArray(brush_stroke_position_attrib);
+  gl.uniform4fv(brush_stroke_color_uniform, [1,0.6,1,1]);
+
   gl.bindBuffer(gl.ARRAY_BUFFER, pencil_stroke_buffer);
   gl.vertexAttribPointer(brush_stroke_position_attrib, 2, gl.FLOAT, false, 0, 0);
   gl.uniformMatrix4fv(brush_stroke_matrix_uniform, false, world_to_clip);
@@ -371,7 +550,7 @@ function FrameUpdate()
 window.addEventListener("resize", () => {
   canvas.height = window.innerHeight;
   canvas.width = window.innerWidth;
-  gl.viewport(0, 0, canvas.width, canvas.height);
+  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
   pixel_to_clip = mat4.create();
   mat4.scale(pixel_to_clip, pixel_to_clip, [canvas.width/2,-canvas.height/2,1]);
   mat4.translate(pixel_to_clip, pixel_to_clip, [1,-1,0]);
