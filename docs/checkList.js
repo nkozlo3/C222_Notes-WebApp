5521fcbe0736132041c1d79ade54a375432f9de4
AnanthHariharan <80168336+AnanthHariharan@users.noreply.github.com>
2023-04-07 11:49:49 -0500

    New functions to edit, drag, drop and filter items

--- docs/checkList.js
+++ docs/checkList.js
@@ -86,12 +86,90 @@ function addListItem(listString) {
   };
 }
 
+// function to edit a list item
+function editListItem(item) {
+  var listString = item.innerHTML;
+  var input = document.createElement("input");
+  input.value = listString;
+  item.innerHTML = "";
+  item.appendChild(input);
+  input.focus();
+  input.addEventListener("keypress", function (e) {
+    if (e.keyCode === 13) {
+      saveListItem(item, input.value);
+    }
+  });
+  input.addEventListener("blur", function () {
+    saveListItem(item, input.value);
+  });
+}
+
 // function to save a list item to local storage with one argument, the list string
-function saveListItem(listString) {
-  // get the list from local storage
-  checkListArray.push(listString);
-  // save the updated list to local storage
+function saveListItem(item, listString) {
+  if (listString == "") {
+    item.remove();
+    return;
+  }
+  item.innerHTML = listString;
+  var index = checkListArray.indexOf(listString);
+  if (index > -1) {
+    checkListArray[index] = listString;
+  } else {
+    checkListArray.push(listString);
+  }
   localStorage.setItem("checkListArray", JSON.stringify(checkListArray));
+}
+
+// event listener for dragstart
+function dragStart(e) {
+  e.dataTransfer.setData("text/plain", e.target.innerHTML);
+  e.dataTransfer.effectAllowed = "move";
+}
+
+// event listener for dragover
+function dragOver(e) {
+  e.preventDefault();
+  e.dataTransfer.dropEffect = "move";
+}
+
+// event listener for drop
+function drop(e) {
+  e.preventDefault();
+  var data = e.dataTransfer.getData("text/plain");
+  e.target.innerHTML = data;
+}
+
+// event listener for dragend
+function dragEnd(e) {
+  // update the checkListArray and local storage accordingly
+  var items = document.querySelectorAll("#checkList li");
+  checkListArray = [];
+  for (var i = 0; i < items.length; i++) {
+    checkListArray.push(items[i].innerHTML);
+  }
+  localStorage.setItem("checkListArray", JSON.stringify(checkListArray));
+}
+
+function filterList() {
+  // get the keyword from the search input
+  var keyword = document.getElementById("searchInput").value.toLowerCase();
+  // get all the list items
+  var items = document.querySelectorAll("#checkList li");
+
+  // loop through each list item
+  for (var i = 0; i < items.length; i++) {
+    var item = items[i];
+    // get the text content of the list item and convert to lowercase
+    var text = item.textContent.toLowerCase();
+    // if the text content contains the keyword
+    if (text.indexOf(keyword) > -1) {
+      // show the list item
+      item.style.display = "";
+    } else {
+      // hide the list item
+      item.style.display = "none";
+    }
+  }
 }
 
 // function to load the list from local storage and add it to the page
@@ -100,6 +178,19 @@ function loadList() {
   if (!localStorage.checkListArray || localStorage.checkListArray == "[]") {
     return;
   }
+  
+  for (var i = 0; i < storedList.length; i++) {
+  var item = document.createElement("li");
+  item.innerHTML = storedList[i];
+  item.draggable = true; // set the draggable attribute to true
+  document.getElementById("checkList").appendChild(item);
+
+  // add event listeners for drag and drop events
+  item.addEventListener("dragstart", dragStart);
+  item.addEventListener("dragover", dragOver);
+  item.addEventListener("drop", drop);
+  item.addEventListener("dragend", dragEnd);
+  
 
   // get the list from local storage
   var storedList = JSON.parse(localStorage.checkListArray);
