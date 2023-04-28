070c9c31a194f649dd9d6c277ae3dad40dc221cc
AnanthHariharan <80168336+AnanthHariharan@users.noreply.github.com>
2023-04-28 13:10:04 -0500

    Update checkList.js

--- docs/checkList.js
+++ docs/checkList.js
@@ -1,155 +1,109 @@
-var checkListArray = [];
+const checkListArray = [];
 
-var listLoaded;
-// on page load
+// Load the list from local storage on page load
 window.onload = function () {
-  // if local storage does not exist or is empty
-  if (!localStorage.checkListArray || localStorage.checkListArray == "[]") {
-    // return
-    return;
-  }
-  // populate the checkListArray with the list from local storage
-  checkListArray = JSON.parse(localStorage.getItem("checkListArray"));
-  // load the list from local storage
-  listLoaded = false;
-};
-
-// on click of 'newCheckBoxButton' button
-document.getElementById("newCheckBoxButton").onclick = function () {
-  // load the list from local storage
-  if (!listLoaded) {
+  const storedList = JSON.parse(localStorage.getItem("checkListArray"));
+  if (Array.isArray(storedList) && storedList.length) {
+    checkListArray.push(...storedList);
     loadList();
-    listLoaded = true;
   }
 };
 
-// on click of 'clearStorage' button
-document.getElementById("clearStorage").onclick = function () {
-  // clear the list from local storage
-  localStorage.clear();
-  // clear the list from the page
-  document.getElementById("checkList").innerHTML = "";
-};
-
-// on click of 'addItem' button
-document.getElementById("checkListAdd").onclick = function () {
-  // the new list string
-  var listString = document.getElementById("checkListTitle").value;
+// Add a new list item on click of 'newCheckBoxButton' button
+document.getElementById("newCheckBoxButton").onclick = function () {
+  const input = document.getElementById("checkListTitle");
+  const listString = input.value.trim();
+  input.value = "";
   addListItem(listString);
 };
 
-// function to save a add a new list item
+// Clear the list from local storage and the page on click of 'clearStorage' button
+document.getElementById("clearStorage").onclick = function () {
+  localStorage.removeItem("checkListArray");
+  document.getElementById("checkList").innerHTML = "";
+};
+
+// Add a new list item on click of 'addItem' button
+document.getElementById("checkListAdd").onclick = function () {
+  const input = document.getElementById("checkListTitle");
+  const listString = input.value.trim();
+  input.value = "";
+  addListItem(listString);
+};
+
+// Add a new list item
 function addListItem(listString) {
-  // if the input is empty
-  if (listString == "") {
-    // do nothing
+  if (listString === "") {
     return;
   }
-  // save the list item to local storage
-  saveListItem(listString);
-
-  // create a list item
-  var item = document.createElement("li");
-  item.innerHTML = listString;
-  // create a close button
-  var close = document.createElement("button");
-  // set text for the close button to be an 'X'
-  close.innerHTML = "X";
-  close.className = "close";
-  item.appendChild(close);
-
-  // on click of the close button
-  close.onclick = function () {
-    // get the index of the list item
-    var index = checkListArray.indexOf(listString);
-    // remove the list item from the list
-    item.remove();
-  };
-
-  // add the list item to the list
+  checkListArray.push(listString);
+  saveListToLocalStorage();
+  const item = createListItem(listString);
   document.getElementById("checkList").appendChild(item);
-  input.value = "";
-
-  // clear the input
-  document.getElementById("checkListTitle").value = "";
-
-  // on click of a list item
-  item.onclick = function () {
-    // if the item is checked
-    if (item.style.textDecoration == "line-through") {
-      // uncheck the item
-      item.style.textDecoration = "none";
-    } else {
-      // check the item
-      item.style.textDecoration = "line-through";
-    }
-  };
 }
 
-// function to edit a list item
+// Edit a list item on double click
 function editListItem(item) {
-  var listString = item.innerHTML;
-  var input = document.createElement("input");
+  const listString = item.textContent;
+  const input = document.createElement("input");
+  input.type = "text";
   input.value = listString;
   item.innerHTML = "";
   item.appendChild(input);
   input.focus();
-  input.addEventListener("keypress", function (e) {
+  input.addEventListener("keyup", function (e) {
     if (e.keyCode === 13) {
-      saveListItem(item, input.value);
+      saveListItem(item, input.value.trim());
     }
   });
   input.addEventListener("blur", function () {
-    saveListItem(item, input.value);
+    saveListItem(item, input.value.trim());
   });
 }
 
-// function to save a list item to local storage with one argument, the list string
+// Save a list item to local storage
 function saveListItem(item, listString) {
-  if (listString == "") {
+  if (listString === "") {
     item.remove();
     return;
   }
   item.innerHTML = listString;
-  var index = checkListArray.indexOf(listString);
+  const index = checkListArray.indexOf(listString);
   if (index > -1) {
     checkListArray[index] = listString;
   } else {
     checkListArray.push(listString);
   }
+  saveListToLocalStorage();
+}
+
+// Create a list item element
+function createListItem(listString) {
+  const item = document.createElement("li");
+  item.textContent = listString;
+  const closeButton = document.createElement("button");
+  closeButton.textContent = "X";
+  closeButton.className = "close";
+  closeButton.onclick = function () {
+    const index = checkListArray.indexOf(listString);
+    checkListArray.splice(index, 1);
+    saveListToLocalStorage();
+    item.remove();
+  };
+  item.appendChild(closeButton);
+  item.ondblclick = function () {
+    editListItem(item);
+  };
+  item.onclick = function () {
+    item.classList.toggle("checked");
+  };
+  return item;
+}
+
+// Save the current list to local storage
+function saveListToLocalStorage() {
   localStorage.setItem("checkListArray", JSON.stringify(checkListArray));
 }
-
-// event listener for dragstart
-function dragStart(e) {
-  e.dataTransfer.setData("text/plain", e.target.innerHTML);
-  e.dataTransfer.effectAllowed = "move";
-}
-
-// event listener for dragover
-function dragOver(e) {
-  e.preventDefault();
-  e.dataTransfer.dropEffect = "move";
-}
-
-// event listener for drop
-function drop(e) {
-  e.preventDefault();
-  var data = e.dataTransfer.getData("text/plain");
-  e.target.innerHTML = data;
-}
-
-// event listener for dragend
-function dragEnd(e) {
-  // update the checkListArray and local storage accordingly
-  var items = document.querySelectorAll("#checkList li");
-  checkListArray = [];
-  for (var i = 0; i < items.length; i++) {
-    checkListArray.push(items[i].innerHTML);
-  }
-  localStorage.setItem("checkListArray", JSON.stringify(checkListArray));
-}
-
 function filterList() {
   // get the keyword from the search input
   var keyword = document.getElementById("searchInput").value.toLowerCase();
